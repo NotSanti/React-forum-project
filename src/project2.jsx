af534c0a6d2defdb748dd8a5d944a04e19e38dd4
Santiago Luna <santiluna03@outlook.com>
2021-11-25 17:06:49 -0500

    update fetch

--- src/project2.jsx
+++ src/project2.jsx
@@ -2,16 +2,16 @@
 
 let global = {};
 function setup() {
-    global.categories = [];
-    let path = 'data/forum.json'
-    fetch(path)
-    .then( resp => resp.json())
-    .then (jsonResp => {
-        console.log(jsonResp);
-        jsonResp.forEach(x => {
-            console.log(x)
-        });
-    } );
+  global.categories = [];
+  let path = "data/forum.json";
+  fetch(path)
+    .then((resp) => resp.json())
+    .then((jsonResp) => {
+      console.log(jsonResp);
+      global.cat1 = jsonResp.categories[0];
+      global.cat2 = jsonResp.categories[1];
+      global.cat3 = jsonResp.categories[2];
+    });
 }
 
 setup();
