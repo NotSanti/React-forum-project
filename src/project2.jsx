0e1eaa5b43133b6d976d23ff69df65b65b90e691
Santiago Luna <santiluna03@outlook.com>
2021-11-30 16:02:05 -0500

    adding lhs buttons component

--- src/project2.jsx
+++ src/project2.jsx
@@ -1,17 +1,42 @@
 "use strict";
 
 let global = {};
+global.LHS = document.querySelector("#LHS");
+
 function setup() {
+  getData();
+}
+
+function getData() {
   global.categories = [];
   let path = "data/forum.json";
   fetch(path)
     .then((resp) => resp.json())
     .then((jsonResp) => {
-      console.log(jsonResp);
-      global.cat1 = jsonResp.categories[0];
-      global.cat2 = jsonResp.categories[1];
-      global.cat3 = jsonResp.categories[2];
+      // console.log(jsonResp);
+      global.cat = jsonResp;
+      renderLHS(jsonResp.categories);
     });
+}
+
+function renderLHS(json) {
+  console.log(json);
+  ReactDOM.render(<Buttons categories={json} />, global.LHS);
+}
+
+class Buttons extends React.Component {
+  render() {
+    let cat = this.props.categories;
+    return (
+      <div>
+        {cat.map((cats, index) => (
+          <button key={index} id={cats.id}>
+            {cats.name}
+          </button>
+        ))}
+      </div>
+    );
+  }
 }
 
 setup();
