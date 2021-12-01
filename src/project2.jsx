81e3d27f79db04233a916d807488c69f4d78cb84
Matthew Toledo <matthew.toledo@dawsoncollege.qc.ca>
2021-11-30 20:34:56 -0500

    restructured code and renamed some variables

--- src/project2.jsx
+++ src/project2.jsx
@@ -1,11 +1,14 @@
 "use strict";
 
-let global = {};
-global.LHS = document.querySelector("#LHS");
+let global;
+
 
 function setup() {
   const middle = document.querySelector("#Middle");
   getData();
+  global = {};
+  global.LHS = document.querySelector("#LHS");
+  global.categories = [];
   // ReactDOM.render(
   //   <Middle listposts={global.cat.categories[0].topicList} />,
   //   middle
@@ -13,13 +16,13 @@ function setup() {
 }
 
 function getData() {
-  global.categories = [];
+  
   let path = "data/forum.json";
   fetch(path)
     .then((resp) => resp.json())
     .then((jsonResp) => {
       // console.log(jsonResp);
-      global.cat = jsonResp;
+      global.categories = jsonResp.categories;
       renderLHS(jsonResp.categories);
     });
 }
@@ -49,17 +52,17 @@ class LHS extends React.Component {
     console.log("say hi");
   }
   render() {
-    let cat = this.props.categories;
+    let category = this.props.categories;
     return (
       <div>
         <div>
-          {cat.map((cats, index) => (
+          {category.map((cats, index) => (
             <button key={index} id={cats.id} onClick={this.handleClick}>
               {cats.name}
             </button>
           ))}
         </div>
-        {this.state.click ? <Middle /> : null}
+        {this.state.click ? <Middle  /> : null}
       </div>
     );
   }
@@ -74,14 +77,14 @@ class Middle extends React.Component {
   }
 
   getData() {
-    let topicList = global.cat[0].topicList;
+    let topicList = global.categories[0].topicList;
     console.log(topicList);
     this.setState({ topics: topicList });
   }
 
   render() {
     console.log("render middle");
-    let listPost = this.state.topics;
+    let listPost = global.categories[0].topicList;
     return (
       <div>
         <p>test render</p>
